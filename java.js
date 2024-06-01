bcb1b5b05e664e5573634bb695cea77d4bf392c6
sivanandagnaneswar <128624565+sivanandagnaneswar@users.noreply.github.com>
2024-06-01 19:12:02 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const navbar = document.getElementById('navbar');
+    
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 50) {
+            navbar.classList.add('scrolled');
+        } else {
+            navbar.classList.remove('scrolled');
+        }
+    });
+});
+
+
